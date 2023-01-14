ab1d690031a2854e50433222e8752d13ffd7ed46
alvinscheibe <alvin.scheibe@gmail.com>
2023-01-14 17:39:22 -0300

    Partial contract code

--- contracts/BuyMeACrypto.sol
+++ contracts/BuyMeACrypto.sol
@@ -2,7 +2,26 @@
 pragma solidity 0.8.17;
 
 contract BuyMeACrypto {
-    function BuyMeACrypto(){
+    //Event to emit when a Memo is created
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
 
+    //Memo struct
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    Memo[] memos; //List of all memos received
+    address payable owner; //Address of contract deployer
+
+    constructor() {
+        owner = payable(msg.sender);
     }
 }
