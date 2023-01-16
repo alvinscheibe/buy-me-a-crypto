05d7d88dfdbe3adee407bde27fad19e07619da5f
alvinscheibe <alvin.scheibe@gmail.com>
2023-01-16 20:42:42 -0300

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const privateKey = process.env.ACCOUNT_PRIVATE_KEY as string;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [
+        privateKey
+      ]
+    }
+  }
 };
 
 export default config;
