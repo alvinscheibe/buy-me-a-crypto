e704c61b91bbc9cee8e4956cb5822b73945e4531
alvinscheibe <alvin.scheibe@gmail.com>
2023-01-18 20:10:52 -0300

    Get balance function in withdraw script

--- scripts/withdraw.ts
+++ scripts/withdraw.ts
@@ -1,7 +1,13 @@
 import hre from "hardhat";
 // @ts-ignore
 import buyMeACryptoABI from '../artifacts/contracts/BuyMeACrypto.sol/BuyMeACrypto.json';
+import { UrlJsonRpcProvider } from '@ethersproject/providers/src.ts/url-json-rpc-provider';
 require('dotenv').config();
+
+async function getBalance(provider: UrlJsonRpcProvider, address: string) {
+  const balanceBigInt = await provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
 
 async function main() {
   const contractAddress = process.env.CONTRACT_ADDRESS;
